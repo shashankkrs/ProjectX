75496d49185d274f01359bc2babd1630b657c40e
shashankkrs <shashankrajput2812@gmail.com>
2023-01-29 13:24:37 +0530

    Defect Memo routes added

--- server.js
+++ server.js
@@ -4,15 +4,17 @@ const app = express()
 const port = 3000
 
 const Driver= require('./model/driver');
-const DefectMemo=require('./model/defectmemo');
 const bodyParser=require('body-parser');
 const Duty_Log=require('./model/duty_log');
 const vehicleRoute=require('./routes/vehicles');
+const defectMemoRoute = require('./routes/defectmemos');
 
 app.use(bodyParser.urlencoded({extended:true}));
 
 
 app.use('/vehicles',vehicleRoute);
+
+app.use('/memo',defectMemoRoute);
 
 
 /*
@@ -126,55 +128,7 @@ app.get('/duty_log/vehicle/:vehicle_id', async(req, res) => {
     }
 });
 
-// To Search Memo
 
-
-app.get('/memo/:id', async(req, res) => {
-    try {
-        const memoID=req.params.id;
-        const foundDefect=await DefectMemo.findById(memoID);
-        res.send(foundDefect);
-    } catch (error) {
-        console.log(error);
-    }
-});
-
-// To Search Memo By Vehicle Id
-
-app.get('/memo/vehicle/:vehicle_id',async(req,res)=>{
-    try{
-        const vehicleId = req.params.vehicle_id;
-        const foundVehicleMemo = await Memo.find({vehicle_id:vehicleId});
-        res.send(foundVehicleMemo);
-    }catch(err){
-        console.log(err);
-    }
-})
-
-// To Add Memo
-app.post('/memo/add', async(req, res) => {
-    try {
-        const newDefectMemo=await new DefectMemo(req.body);
-        newDefectMemo.save();
-        if (newDefectMemo) {
-            res.send("New Memo Added");
-        }else{
-            res.send("No Memo Added");
-        }
-    } catch (error) {
-        console.log(error);
-    }
-});
-
-// To Show Memo
-app.get('/memo', async(req, res) => {
-    try {
-        const newDefectMemo=await DefectMemo.find();
-        res.send(newDefectMemo);
-    } catch (error) {
-        console.log(error);
-    }
-});
 
 // To find drivers
 app.get('/drivers/:id', async(req, res) => {
