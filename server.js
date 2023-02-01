37dc225e3beab5130fd622ac2919a962afc5decb
shashankkrs <shashankrajput2812@gmail.com>
2023-02-01 23:50:18 +0530

    Minor Fix

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 //Using Important Middlewares
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(cors({
