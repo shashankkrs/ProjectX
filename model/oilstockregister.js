3f13761aeb240e83cf4f836edc47a167de0409bd
VISHAL <vishalvishnu0657@gmail.com>
2023-03-27 23:09:37 +0530

    oilresgister model created

---
+++ model/oilstockregister.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const oilregister_schema=mongoose.Schema({
+    Name_of_store:String,
+    Date:Date,
+    Voucher_no:String,
+    from_whom_received_to_whom_issued:String,
+    recived:Number,
+    issued:Number,
+    balance:Number,
+    signature_of_pol_havaldar:Boolean,
+    signature_of_pol_havaldar:Boolean,
+    Remarks:String,
+
+
+})
+const oilregister_Model=mongoose.model('oilregister',oilregister_schema)
+module.exports=oilregister_Model;
